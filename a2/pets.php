606448f7804a6f44d5ca6658be01fab22739ebdc
s3921946 <s3921946@student.rmit.edu.au>
2024-09-13 09:39:32 +1000

    Configure pets.php with includes

--- a2/pets.php
+++ a2/pets.php
@@ -1,39 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PETS</title>
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <script src="main.js"></script>
-</head>
-
-<body>
-    <div class="flexbox-container">
-
-        <header class="flexbox-item-header">
-            <div class="header-container">
-                <img class="img-logo" src="../images/logo.png" alt="Logo">
-                <div class="custom-select">
-                    <select id="navigation-select">
-                        <option value="">Select an Option:</option>
-                        <option value="index.html">index</option>
-                        <option value="add.html">add</option>
-                        <option value="gallery.html">gallery</option>
-                        <option value="pets.html">pets</option>
-                    </select>
-                </div>
-                <div class="navigation-search">
-                    <form action="/action_page.php">
-                        <input type="text" placeholder="Search" name="Search">
-                        <span class="material-symbols-outlined">search</span>
-                    </form>
-                </div>
-            </div>
-        </header>
-
-        <!-- Replace flexbox-item-centre-1 with main -->
+<?php
+$title = "index";
+include 'includes/header.inc';
+include 'includes/nav.inc';
+include 'includes/db_connect.inc';
+?>
         <main class="flexbox-item-centre-1">
             <div class="flexbox-pets pets-centre-top">
                 <h1 class="pets-heading">Discover Pets Victoria</h1>
@@ -45,7 +15,7 @@
                 <h5 class="pets-description">education, and community support programs, all aimed at promoting responsible pet ownership and reducing the number of homeless animals.</h5>
             </div>
             <div class="flexbox-pets pets-centre-bottom">
-                <img class="pets-center-image" src="../images/pets.jpeg" alt="Pets Image">
+                <img class="pets-center-image" src="images/pets.jpeg" alt="Pets Image">
                 <table class="pets-content-table">
                     <thead>
                         <tr>
@@ -97,9 +67,7 @@
             </div>
         </main>
 
-        <footer class="flexbox-item-footer">
-            <p class="paragraph-copyright">© COPYRIGHT s3921946. All rights Reserved | Designed for Pets Victoria</p>
-        </footer>
-    </div>
-</body>
+        <?php
+        include 'includes/footer.inc';
+        ?>
 </html>
