32e4c8bbc224eea96a3a84850c89811356e706ab
s3921946 <s3921946@student.rmit.edu.au>
2024-10-01 16:39:26 +1000

    Finalizing add.process-php

--- a2/add-process.php
+++ a2/add-process.php
@@ -23,3 +23,40 @@ if (isset($_FILES['pet-image']) && $_FILES['pet-image']['error'] == 0) {
 } else {
     echo "There was a problem with the file. Click here to go back.";
 }
+
+if ($error == 0) {
+    $sql = "INSERT INTO pets (petname, type, description, image, caption, age, location) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param(
+      "sssssis",
+      $_POST['petname'], 
+      $_POST['pet-type'], 
+      $_POST['description'], 
+      $_FILES['pet-image']['name'], 
+      $_POST['image-cap'],
+      $_POST['age-months'],
+      $_POST['location'] );
+    $stmt->execute();
+    print_r($stmt->error);
+
+    if ($stmt->affected_rows > 0) {
+        echo "<p>Pets was added to the database.</p>";
+             if (move_uploaded_file($_FILES['pet-image']['tmp_name'], $imagePath . "/" . $_FILES['pet-image']['name'])) {
+                echo "<p>File was added to the images directory.</p>";
+                } else {
+                    echo "<p>File was NOT added to the images directory.</p>";
+                }
+            } else {
+            echo "<p>Country was not added to the database, image not uploaded.</p>";
+        }
+    } else {
+        echo "<p>There is a problem with the image file.</p>";
+    }
+        echo "<p>Return to <a href='gallery.php'>Pets Gallery Page</a>.</p>";
+
+echo "\nInsert Data  Successful!";
+
+
+include 'includes/footer.inc';
+
+?>
