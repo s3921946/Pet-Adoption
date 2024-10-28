6c1fc9fe32b8facc44939fafd0116afd6edf09e7
s3921946 <s3921946@student.rmit.edu.au>
2024-10-28 20:04:15 +1100

    Update edit-process.php

--- a3/edit-process.php
+++ a3/edit-process.php
@@ -4,6 +4,8 @@ include 'includes/header.inc';
 include 'includes/nav.inc';
 include 'includes/db_connect.inc';
 
+$sql = null;
+
 $petid = $_POST['petid'];
 $petname = $_POST['petname'];
 $pet_type = $_POST['type'];
