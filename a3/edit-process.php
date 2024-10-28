dc374d6f9afa5948aedd99b17815dc7715393f44
s3921946 <s3921946@student.rmit.edu.au>
2024-10-28 19:35:09 +1100

    Update edit-process.php

--- a3/edit-process.php
+++ a3/edit-process.php
@@ -4,16 +4,16 @@ include 'includes/header.inc';
 include 'includes/nav.inc';
 include 'includes/db_connect.inc';
 
-$petname = $_POST['name'];
-$pet_type = $_POST['pet-type'];
+$petname = $_POST['petname'];
+$pet_type = $_POST['type'];
 $description = $_POST['description'];
-$caption = $_POST['image-cap'];
-$age = (double)$_POST['age-months'];
+$caption = $_POST['caption'];
+$age = (double)$_POST['age'];
 $location = $_POST['location'];
 
-$image = $_FILES['pet-image']['name'];
-$temp = $_FILES['pet-image']['tmp_name'];
-$error = $_FILES['pet-image']['error'];
+$image = $_FILES['image']['name'];
+$temp = $_FILES['image']['tmp_name'];
+$error = $_FILES['image']['error'];
 
 
 if (isset($_FILES['pet-image']) && $_FILES['pet-image']['error'] == 0) {
