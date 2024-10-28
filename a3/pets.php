d4748771a46b63c3680f2c08a9699f075cebe536
s3921946 <s3921946@student.rmit.edu.au>
2024-10-28 15:24:23 +1100

    Update pets.php

--- a3/pets.php
+++ a3/pets.php
@@ -6,7 +6,7 @@ include 'includes/db_connect.inc';
 ?>
     <main class="container-fluid">
         <div class="row text-center mt-4 ysabeau-SC ">
-            <h2 class="fw-bold">Discover Pets Victoria</h1>
+            <h2 class="fw-bold">Discover Pets Victoria</h2>
             <p>Pets Victoria is a dedicated pet adoption organization based in Victoria, Australia, focused on providing a safe and loving environment for pets in need. With a compassionate approach, Pets Victoria works tirelessly to rescue, rehabilitate, and rehome dogs, cats, and other animals. Their mission is to connect these deserving pets with caring individuals and families, creating lifelong bonds. The organization offers a range of services, including adoption counseling, pet education, and community support programs, all aimed at promoting responsible pet ownership and reducing the number of homeless animals.</p>
         </div>
     </main>
